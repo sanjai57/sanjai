76e017fcdf7d2ef3a5ab0baf2d2f0c59847f8cab
sanjai57 <122263883+sanjai57@users.noreply.github.com>
2023-01-11 07:58:52 +0530

    Add files via upload

---
+++ program6 day2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def letterCombinations(self, digits):
+        if(len(digits) == 0):
+            return []
+
+        digit2char = {'1': '',     '2': 'abc', '3': 'def',
+                      '4': 'ghi',  '5': 'jkl', '6': 'mno',
+                      '7': 'pqrs', '8': 'tuv', '9': 'wxyz', '0': ''}
+        resus = ['']
+
+        for d in digits:
+            tem = []
+            for c in digit2char[d]:
+                tem = tem = [r + c for r in resus]
+
+            resus = tem
+
+        return resus
+
+ob = Solution()
+print(ob.letterCombinations('87'))
+        
+
+
+    
