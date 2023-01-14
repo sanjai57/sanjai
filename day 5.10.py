3efbabd14b177b1c7e264080c14a85f635201718
sanjai57 <122263883+sanjai57@users.noreply.github.com>
2023-01-14 08:33:56 +0530

    Add files via upload

---
+++ day 5.10.py
@@ -0,0 +1,9 @@
+string = "be good and do good"
+# reversing words in a given string
+s = string.split()[::-1]
+l = []
+for i in s:
+    # apending reversed words to l
+    l.append(i)
+# printing reverse words
+print(" ".join(l))
